92016566fc71d0e06e909253b74e00325e1b7044
Thein Naing <theinnaing0908@gmail.com>
2023-04-25 16:39:59 +0800

    modified return functiion of renderSquare

--- src/components/Board.js
+++ src/components/Board.js
@@ -40,29 +40,33 @@ export default function Board () {
     onClick={() => handleClick(i)} />
   }
 
+    return (
+      <div classname="board">
+      <div classname="board-row">
+      {renderSquare(0)}
+      {renderSquare(1)}
+      {renderSquare(2)}
 
+      </div>
+      <div classname="board-row">
+      {renderSquare(3)}
+      {renderSquare(4)}
+      {renderSquare(5)}
+      </div>
+      <div classname="board-row">
+      {renderSquare(6)}
+      {renderSquare(7)}
+      {renderSquare(8)}
+    </div>
+    <div className="status">
+      {status}
+    </div>
+    <button className="reatart" onClick={handleRestart}>
+      Restart Game!
+    </button>
+    </div>
 
-
-return (
-<div classname="board">
-<div classname="board-row">
-<Square value={squares[0]} onClick={()=> handleClick(0)} />
-<Square value={squares[1]} onClick={()=> handleClick(1)} />
-<Square value={squares[2]} onClick={()=> handleClick(2)} />
-</div>
-<div classname="board-row">
-<Square value={squares[3]} onClick={()=> handleClick(3)} />
-<Square value={squares[4]} onClick={()=> handleClick(4)} />
-<Square value={squares[5]} onClick={()=> handleClick(5)} />
-</div>
-<div classname="board-row">
-<Square value={squares[6]} onClick={()=> handleClick(6)} />
-<Square value={squares[7]} onClick={()=> handleClick(7)} />
-<Square value={squares[8]} onClick={()=> handleClick(8)} />
-</div>
-</div>
-
-)}
+    )}
 
 function calculateWinner(squares) {
   const winningPatterns = [
