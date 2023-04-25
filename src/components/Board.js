33949251c790b63a915abf2214fc2a61a4fc0ddb
Thein Naing <theinnaing0908@gmail.com>
2023-04-25 13:54:17 +0800

    update App.css and add handleClick/onClick event listener and useStates function

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,25 +9,31 @@ import { useState } from "react";
 }
 
 export default function Board () {
-  const [sqaures, setSquares] = useState(Array(9).null);
+  const [squares, setSquares] = useState(Array(9).fill(null));
   const [isX, setIsX]  = useState(true);
+  const  handleClick = (i) => {
+    squares[i] = isX ? "X" : "O"
+    setSquares(squares)
+    setIsX(!isX)
+
+  }
 
 return (
 <div classname="board">
 <div classname="board-row">
-<Square value={"X"} />
-<Square value={"0"} />
-<Square value={"X"} />
+<Square value={squares[0]} onClick={()=> handleClick(0)} />
+<Square value={squares[1]} onClick={()=> handleClick(1)} />
+<Square value={squares[2]} onClick={()=> handleClick(2)} />
 </div>
 <div classname="board-row">
-<Square value={"0"} />
-<Square value={"X"} />
-<Square value={"0"} />
+<Square value={squares[3]} onClick={()=> handleClick(3)} />
+<Square value={squares[4]} onClick={()=> handleClick(4)} />
+<Square value={squares[5]} onClick={()=> handleClick(5)} />
 </div>
 <div classname="board-row">
-<Square value={"X"} />
-<Square value={"0"} />
-<Square value={"X"} />
+<Square value={squares[6]} onClick={()=> handleClick(6)} />
+<Square value={squares[7]} onClick={()=> handleClick(7)} />
+<Square value={squares[8]} onClick={()=> handleClick(8)} />
 </div>
 </div>
 
